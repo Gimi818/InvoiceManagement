9f033f824959c2c00752eac465c241d4e675f1f9
Gimi818 <gimi8183@gmail.com>
2023-08-29 21:37:27 +0200

    created client entity

--- src/main/java/com/invoicemanagement/invoice/Invoice.java
+++ src/main/java/com/invoicemanagement/invoice/Invoice.java
@@ -1,4 +1,31 @@
 package com.invoicemanagement.invoice;
 
+import com.invoicemanagement.client.Client;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "invoice")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Invoice {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String invoiceNumber;
+    @ManyToOne
+    private Client client;
+    // @ManyToOne
+    //private Supplier supplier;
+    private LocalDate issueDate;
+    private BigDecimal totalAmount;
 }
