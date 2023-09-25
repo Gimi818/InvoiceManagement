9dec5d9b79855700d07a96877b2d41a79bf2025a
Gimi818 <gimi8183@gmail.com>
2023-09-25 23:53:51 +0200

    added  invoice controller

--- src/main/java/com/invoicemanagement/invoice/InvoiceController.java
+++ src/main/java/com/invoicemanagement/invoice/InvoiceController.java
@@ -1,4 +1,24 @@
 package com.invoicemanagement.invoice;
 
+import com.invoicemanagement.invoice.dto.InvoiceRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/invoices")
 public class InvoiceController {
+
+    private final InvoiceService service;
+
+    @PostMapping("/add")
+    public ResponseEntity<Invoice> saveInvoice(@RequestBody InvoiceRequestDto invoiceRequestDto) {
+        return new ResponseEntity<>(service.saveInvoice(invoiceRequestDto), HttpStatus.CREATED);
+    }
+
 }
