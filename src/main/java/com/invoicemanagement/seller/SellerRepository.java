d5b5c3c80f5b2f5344c7b3e23ff3e3fdd214eab3
Gimi818 <gimi8183@gmail.com>
2023-10-12 22:42:34 +0200

    error correction in repository

--- src/main/java/com/invoicemanagement/seller/SellerRepository.java
+++ src/main/java/com/invoicemanagement/seller/SellerRepository.java
@@ -2,5 +2,5 @@ package com.invoicemanagement.seller;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SellerRepository extends JpaRepository<Long, Seller> {
+public interface SellerRepository extends JpaRepository< Seller, Long> {
 }
