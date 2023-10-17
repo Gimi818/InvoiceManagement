3bd21dface4ca54da7d8f63f2cf4ff04b889b8f7
Gimi818 <gimi8183@gmail.com>
2023-10-17 21:27:34 +0200

    added method find by id in BuyerService

---
+++ src/main/java/com/invoicemanagement/buyer/BuyerService.java
@@ -0,0 +1,17 @@
+package com.invoicemanagement.buyer;
+
+import com.invoicemanagement.buyer.dto.BuyerResponseDto;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class BuyerService {
+    private final BuyerRepository repository;
+
+    public BuyerResponseDto findBuyerById(Long id) {
+        Buyer buyer = repository.findById(id).orElseThrow();
+        return mapper.entityToDto(buyer);
+    }
+
+}
